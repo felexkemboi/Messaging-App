c7bd6e6f1dd3726720a11be5874f48ece2d529b9
felexkemboi <felokemboi10@gmail.com>
2018-03-30 01:38:25 +0300

    create models for the posts app

--- messaging/posts/models.py
+++ messaging/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.config import settings
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+	message = models.CharField(max_length=280)
+	date = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+
+	def get_absolute_url(self):
+		return reverse('post_detail',args =[str(self.id)])
+
+	def __str__(self):
+		return self.message
