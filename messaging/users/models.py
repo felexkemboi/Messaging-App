edc5537c36518f342a74d331f6b6ea0604bd2ce0
felexkemboi <felokemboi10@gmail.com>
2018-03-29 17:38:57 +0300

    add user model to the database

--- messaging/users/models.py
+++ messaging/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.cotrib.auth.models import AbstracUser,UserManager 
+
+class CustomUserManager(UserManager):
+	pass
+
+class CustomUser(AbstrsctUser):
+	objects = CustomUserManager()
+		
+
