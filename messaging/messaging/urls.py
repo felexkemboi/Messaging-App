10996c70e3e8c290970d0513086074bb223e1eec
felexkemboi <felokemboi10@gmail.com>
2018-03-30 01:56:55 +0300

    add url to redirect urls for requests related to the posts

--- messaging/messaging/urls.py
+++ messaging/messaging/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',       include('pages.urls')),
+    path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
