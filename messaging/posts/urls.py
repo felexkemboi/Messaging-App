6de601c3355890e94d6721bb628e41005a7c9200
felexkemboi <felokemboi10@gmail.com>
2018-03-30 02:33:27 +0300

    add commas after every path in the post/urls.py

--- messaging/posts/urls.py
+++ messaging/posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.PostListView.as_view(),name='post_list'),
-  path('<int:pk>/edit/',views.PostUpdateView.as_view(), name = 'post_edit')
-  path('<int:pk>/delete/',views.DeleteView.as_view(),   name = 'post_delete')
-  path('<int:pk>/',views.PostDetailView.as_view() ,     name = 'post_detail')
+  path('',                 views.PostListView.as_view(),         name='post_list'),
+  path('<int:pk>/edit/',   views.PostUpdateView.as_view(),       name = 'post_edit'),
+  path('<int:pk>/delete/', views.DeleteView.as_view(),          name = 'post_delete'),
+  path('<int:pk>/',        views.PostDetailView.as_view() ,     name = 'post_detail'),
 ]
