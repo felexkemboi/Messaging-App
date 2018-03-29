26f910f68f4a6253c1b816466753a461455b25eb
felexkemboi <felokemboi10@gmail.com>
2018-03-29 22:36:09 +0300

    add homepage view to the pages app.Use genric views method to render the template

--- messaging/pages/views.py
+++ messaging/pages/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import path
+from django.views.generic import TemplateView
 
-from . import views
-
-urlpatterns = [
- path('',views.HomePageView.as_view(),name='home'),
-]
+class HomePageView(TemplateView):
+	template_name = 'home.html'
