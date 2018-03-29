82d9cf079e9f80bb4b9619937c44a6b65069392f
felexkemboi <felokemboi10@gmail.com>
2018-03-30 02:22:01 +0300

    add urls to handle editing,details and deletion of a post

--- messaging/posts/urls.py
+++ messaging/posts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.PostListView.as_view(),name='post_list'),
+  path('<int:pk>/edit/',views.PostUpdateView.as_view(), name = 'post_edit')
+  path('<int:pk>/delete/',views.DeleteView.as_view(),   name = 'post_delete')
+  path('<int:pk>/',views.PostDetailView.as_view() ,     name = 'post_detail')
 ]
