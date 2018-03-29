0698c05f342ba7cd78ef920f3d3e8f946a2269d0
felexkemboi <felokemboi10@gmail.com>
2018-03-29 22:28:54 +0300

    add homepage view to the new pages app.The views are not ready yet

--- messaging/pages/views.py
+++ messaging/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+ path('',views.HomePageView.as_view(),name='home'),
+]
