b96fe2ac726e866a6d84018f76df147e126e7d01
felexkemboi <felokemboi10@gmail.com>
2018-03-30 02:30:03 +0300

    add the views for update,deletion and for checking the details of each view

--- messaging/posts/views.py
+++ messaging/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 from . import models
@@ -7,3 +9,17 @@ from . import models
 class PostListView(ListView):
 	model = models.Post
 	template_name = 'post_list.html'
+
+class PostDetailView(DetailView):
+	model = models.Post
+	template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+	model = models.Post
+	fields = ['message']
+	template_name = 'post_edit.html'
+
+class PostDeleteView(ListView):
+	model = models.Post
+	template_name = 'post_delete.html'
+	success_url = reverse_lazy('post_list')
