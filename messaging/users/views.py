0817c8a8961984afea62e162f3e764344a04a206
felexkemboi <felokemboi10@gmail.com>
2018-03-29 21:44:43 +0300

    add a class view for the signup

--- messaging/users/views.py
+++ messaging/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
 
 # Create your views here.
+
+from .forms import CustomerUserCreationForm
+
+class SignUp(generic.CreateView):
+	form_class = CustomerUserCreationForm
+	success_url = reverse_lazy('login')
+	template_name = 'signup.html'
